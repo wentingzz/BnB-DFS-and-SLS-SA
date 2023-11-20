c86719b8cf974dc614212b34dfa014139590521e
Wenting <wenttzzheng@gmail.com>
2023-11-20 13:24:42 -0800

    Adds function to calculate total distance given solution

--- SLS/main.py
+++ SLS/main.py
@@ -5,3 +5,8 @@ def read_distance_matrix(file_path):
     # Load the distance matrix from the file
     distance_matrix = np.loadtxt(file_path, delimiter=" ", skiprows=1)
     return distance_matrix
+def calculate_total_distance(solution, distance_matrix):
+    total_distance = distance_matrix[solution[0], solution[-1]]
+    for i in range(len(solution) - 1):
+        total_distance += distance_matrix[solution[i], solution[i + 1]]
+    return total_distance
